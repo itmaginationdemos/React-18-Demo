2953bffe6e48c6776f87fab4a953f84dee1537ec
Aleksander <aleksander.jess@itmagination.com>
2021-09-11 15:32:50 +0200

    created a util function

---
+++ src/typedFetch.ts
@@ -0,0 +1,10 @@
+// A workaround
+export function request<TResponse>(
+    url: string,
+    config: RequestInit = {}
+): Promise<TResponse> {
+
+    return fetch(url, config)
+        .then((response) => response.json())
+        .then((data) => data as TResponse);
+}
