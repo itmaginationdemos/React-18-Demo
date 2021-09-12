42539216bdee62f86f892b15c9f24245228d885b
Aleksander <aleksander.jess@itmagination.com>
2021-09-12 20:26:58 +0200

    changed the api url

--- src/exampleComponent.tsx
+++ src/exampleComponent.tsx
@@ -1,34 +1,35 @@
 import React, { useState } from 'react';
 import { request } from './typedFetch'
+
 export default function ExampleComponent() {
 
     const [count, setCount] = useState(0);
     const [error, setError] = useState(false);
     const [joke, setJoke] = useState("")
 
-
     async function handleClick() {
-        request<string>("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt")
+        request<any>("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single")
             .then((res) => {
+                
                 // these two below will be "batched" (grouped) together
                 setCount(count => count + 1)
-                setJoke(res)
+                setJoke(res.joke.toString())
                 // React 18+ will rerender only once at the end.
             })
             .catch(() => {
                 console.log("So this is awkward... Something went wrong.")
                 setError(true)
             })
-            // React 18 will rerender only now
-        }
+        // React 18 will rerender only now
+    }
 
     return (
-        <div style={{display: "flex", flexDirection: "column"}}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
             {error ? <p>Warsaw, we have a problem. Refresh the page to try again.</p> : null}
             <h1>Click to get a random Dad joke, and to increase count</h1>
             <p>Count: {count}</p>
             {joke ? <p>{joke}</p> : <i>Warning the joke may be really bad...</i>}
-            <button onClick={handleClick} style={{width: "3rem", alignSelf: "center", marginTop: "0.5rem"}}>Click</button>
+            <button onClick={handleClick} style={{ width: "3rem", alignSelf: "center", marginTop: "0.5rem" }}>Click</button>
         </div>
     )
 }
