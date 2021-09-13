e967d658c76aa2db46594964deff988957998db5
Aleksander <aleksander.jess@itmagination.com>
2021-09-13 10:45:26 +0200

    changed the test slightly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Click to get a random Dad joke/i);
   expect(linkElement).toBeInTheDocument();
 });
