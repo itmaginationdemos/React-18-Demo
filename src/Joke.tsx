6315f6ad3b596d68007defd5bcd9344f69d72b8e
Aleksander <aleksander.jess@itmagination.com>
2022-04-06 12:54:40 +0200

    added a Joke component

---
+++ src/Joke.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from "react"
+
+interface Props {
+    joke: string
+}
+
+export default function Joke({ joke }: Props) {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+        <div>{joke}</div>
+        </Suspense>
+    )
+}
