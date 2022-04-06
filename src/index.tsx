5789eb8425574b2ff4e6b2ba6ad9064a5ffe10f2
Aleksander <aleksander.jess@itmagination.com>
2022-04-06 13:08:02 +0200

    hydration, bc why not

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,14 +11,14 @@ if (!rootElement) throw new Error("Failed to find the root element")
 
 // to use the new features
 
-const root = ReactDOM.createRoot(rootElement);
+const root = hydrateRoot(rootElement, < App />);
 
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
