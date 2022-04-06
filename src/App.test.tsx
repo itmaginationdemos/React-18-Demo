88a0f9a9c5eaa88893c16371e99bbf0888ce58e5
Aleksander <aleksander.jess@itmagination.com>
2022-04-06 12:54:19 +0200

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Joke from './Joke';
+import { ScriptElementKindModifier } from 'typescript';
 
-test('renders learn react link', () => {
-  render(<App />);
+beforeEach(() => render(<App />))
+
+test('renders', () => {
+
   const linkElement = screen.getByText(/Click to get a random Dad joke/i);
   expect(linkElement).toBeInTheDocument();
 });
